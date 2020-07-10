336e00a941d8053a4c94c78bf029638972fa591d
Maximo <maximo1611@gmail.com>
2020-07-10 03:22:21 -0300

    v1

--- scripts/script-gif.js
+++ scripts/script-gif.js
@@ -8,6 +8,7 @@ const $CAJA_BOTON_GRABAR = document.getElementById('caja-del-boton-grabar');
 const $BOTON_GRABAR = document.getElementById('boton-grabar');
 const $CAJA_SUBIR_GIF = document.getElementById('caja-subir-gif')
 const $CONTADOR = document.getElementById('contador')
+const CONTENEDOR_MIS_GIFOS = document.getElementById('contenedor-mis-gifos')
 if (localStorage.getItem("tema")=== "Tema-Oscuro"){
   
   document.documentElement.style.setProperty("--logo-azul", "url(../imagenes/gifOF_logo_dark.png)")
@@ -120,12 +121,32 @@ navigator.mediaDevices.getUserMedia(
 var buttonSubir = document.getElementById("subir")
 
 buttonSubir.onclick = subirVideo;
-function subirVideo() {
+async function subirVideo() {
   var form = new FormData();
   form.append("file", blob, "file.gif")
-  fetch("https://upload.giphy.com/v1/gifs?&api_key=igY9hZqLYu5goaPfGeVuhM8DmxXLxu3v", {
+  let post =  await fetch("https://upload.giphy.com/v1/gifs?&api_key=igY9hZqLYu5goaPfGeVuhM8DmxXLxu3v", {
+   
       method: "POST",
       body: form
   })
-
+  let id_post =  await post.json()
+  localStorage.setItem("mis-gifs", id_post.data.id)
 }
+
+async function mis_gifos() {
+  let mis_gifs = await fetch(`https://api.giphy.com/v1/gifs/${gifs_pagina}?api_key=KSRFihSLXt224ZjBa5gK4SUm9msngCqt`)
+  try{
+    let gif_por_id = await mis_gifs.json()
+  for (let i = 0; i < localStorage.getItem("mis-gifs").length; i++) {
+    var gifs_pagina = localStorage.getItem("mis-gifs")[i];
+    CONTENEDOR_MIS_GIFOS.innerHTML= `<div class="elemento">
+    <div> 
+    <img src="${gif_por_id.images.original.url}" class="gifos">
+    </div>
+  </div>`
+  }
+  }catch{
+    console.error("Error al conseguir mis guifos")
+  }
+}
+mis_gifos()
